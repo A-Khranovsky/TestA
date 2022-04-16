22e85778697cece6cb2b0688b832bed4488207fc
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-04-16 13:16:28 +0300

    Created TestaRestApiEngineinterface & TestaRestApiEngine services

---
+++ app/Services/TestaRestApiEngine/TestaRestApiEngine.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\TestaRestApiEngine;
+
+class TestaResApiEngine implements TestaResApiEngineinterface
+{
+
+}
---
+++ app/Services/TestaRestApiEngine/TestaRestApiEngineinterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\TestaRestApiEngine;
+
+interface TestaResApiEngineinterface
+{
+
+}
